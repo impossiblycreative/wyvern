3f2d3711387b0483b38a2ed40822dc19dc77386e
Adam Soucie <adamsoucie@gmail.com>
2020-07-13 23:28:26 -0400

    Improving 404 description situation

--- template-parts/page-header/content.php
+++ template-parts/page-header/content.php
@@ -6,17 +6,19 @@
     <?php endif; ?>
 </h1>
 
-<div class="page-header-meta">
-    <?php if ( is_search() ) :
-            $count = $GLOBALS['wp_query']->found_posts;
-            echo esc_html__( 'We found ', 'wyvern' ) . $count . ( $count !== 1 ? esc_html__( ' results', 'wyvern' ) : esc_html__( ' result', 'wyvern' ) );
-        else :
-            $count = $GLOBALS['wp_query']->found_posts;
-            echo $count . ( $count !== 1 ? esc_html__( ' posts', 'wyvern' ) : esc_html__( ' post', 'wyvern' ) );
-        endif;
-    ?>
-</div>
+<?php if ( !is_404() ) : ?>
+    <div class="page-header-meta">
+        <?php if ( is_search() ) :
+                $count = $GLOBALS['wp_query']->found_posts;
+                echo esc_html__( 'We found ', 'wyvern' ) . $count . ( $count !== 1 ? esc_html__( ' results', 'wyvern' ) : esc_html__( ' result', 'wyvern' ) );
+            else :
+                $count = $GLOBALS['wp_query']->found_posts;
+                echo $count . ( $count !== 1 ? esc_html__( ' posts', 'wyvern' ) : esc_html__( ' post', 'wyvern' ) );
+            endif;
+        ?>
+    </div>
 
-<?php if ( get_the_archive_description() ) : ?>
-    <div class="page-header-description"><?php echo wp_kses_post( get_the_archive_description() ); ?></div>
+    <?php if ( get_the_archive_description() ) : ?>
+        <div class="page-header-description"><?php echo wp_kses_post( get_the_archive_description() ); ?></div>
+    <?php endif; ?>
 <?php endif; ?>
